c045a07c4d43118329f410357d1960205cc61dac
GyokayAli <gyokay.metin.ali@gmail.com>
2020-02-03 13:46:05 +0100

    Completed server component

--- src/app/sections/section-health/server/server.component.ts
+++ src/app/sections/section-health/server/server.component.ts
@@ -10,9 +10,28 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
   ngOnInit() {
+    this.setServerStatus(this.serverInput.isOnline);
   }
 
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    this.setServerStatus(!onlineStatus);
+  }
 }
