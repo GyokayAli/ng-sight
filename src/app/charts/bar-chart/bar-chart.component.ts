fe37fea7e7235d752b9d5431322bda17a8a31935
GyokayAli <gyokay.metin.ali@gmail.com>
2020-02-06 21:25:42 +0100

    Populate bar chart from api

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-const SAMPLE_BARCHART_DATA: any[] = [
-  { data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Sales' },
-  { data: [25, 39, 60, 89, 36, 54, 50], label: 'Q4 Sales' }
-];
-
-const SAMPLE_BARCHART_LABELS: string[] = ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7'];
+import { SalesDataService } from 'src/app/services/sales-data.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-bar-chart',
@@ -14,17 +9,53 @@ const SAMPLE_BARCHART_LABELS: string[] = ['Week 1', 'Week 2', 'Week 3', 'Week 4'
 })
 export class BarChartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _salesDataService: SalesDataService) { }
 
-  public barChartData: any[] = SAMPLE_BARCHART_DATA;
-  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
+  orders: any;
+  orderLabels: string[];
+  orderData: number[];
+
+  public barChartData: any[];
+  public barChartLabels: string[];
   public barChartType = 'bar';
   public barChartLegend = true;
   public barChartOptions: any = {
     scaleShowVerticalLines: false,
     responsive: true
   };
+
   ngOnInit() {
+    this._salesDataService.getOrders(1, 100)
+      .subscribe(res => {
+
+        const localChartData = this.getChartData(res);
+        this.barChartLabels = localChartData.map(x => x[0]).reverse();
+        this.barChartData = [{ data: localChartData.map(x => x[1]), label: 'Sales' }];
+      });
   }
 
+  getChartData(res: Response) {
+    this.orders = res['page']['data'];
+    const data = this.orders.map(o => o.total);
+
+    const formattedOrders = this.orders.reduce((r, e) => {
+      r.push([moment(e.placed).format('YY-MM-DD'), e.total]);
+      return r;
+    }, []);
+
+    const temp = [];
+
+    const chartData = formattedOrders.reduce((r, e) => {
+      const key = e[0];
+      if (!temp[key]) {
+        temp[key] = e;
+        r.push(temp[key]);
+      } else {
+        temp[key][1] += e[1]; // add up the order total
+      }
+      return r;
+    }, []);
+
+    return chartData;
+  }
 }
